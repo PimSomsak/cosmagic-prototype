bad1d94e968332d4900d689cefb72d414cc7fb96
neawz <chayapat2549@gmail.com>
2025-10-27 21:29:46 +0700

    27-10-2568
    Active Crushed DONE

--- Assets/Scripts/IngredientChange.cs
+++ Assets/Scripts/IngredientChange.cs
@@ -1,24 +1,13 @@
 using UnityEngine;
-public enum IngredientState { Raw, Crushed }
 
 public class IngredientChange : MonoBehaviour
 {
-    public IngredientState state = IngredientState.Raw;
-
-    public Sprite rawSprite;
-    public Sprite crushedSprite;
-
-    private SpriteRenderer sr;
+    public GameObject crushedSprite;
 
     // How much crush force is needed before state changes
     public int hitsNeeded = 3;
     private int hitCount = 0;
 
-    void Start()
-    {
-        sr = GetComponent<SpriteRenderer>();
-        UpdateSprite();
-    }
     private void OnCollisionEnter2D(Collision2D col)
     {
         if (col.collider.GetComponent<PestleNew>() != null)
@@ -35,11 +24,10 @@ public class IngredientChange : MonoBehaviour
         if (hitCount >= hitsNeeded)
         {
             hitCount = 0;
-            if (state == IngredientState.Raw) state = IngredientState.Crushed;
 
             UpdateSprite();
             Debug.Log($"Ingredient hit {hitCount}/{hitsNeeded}");
-            Debug.Log($"Ingredient state changed to {state}");
+            Debug.Log($"Ingredient changed state");
         }
         else
         {
@@ -49,7 +37,7 @@ public class IngredientChange : MonoBehaviour
 
     private void UpdateSprite()
     {
-        if (state == IngredientState.Raw) sr.sprite = rawSprite;
-        else if (state == IngredientState.Crushed) sr.sprite = crushedSprite;
+        Instantiate(crushedSprite, transform.position, Quaternion.identity);
+        Destroy(gameObject);
     }
 }
