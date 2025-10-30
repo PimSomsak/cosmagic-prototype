b305a63cffddfb193e2953d73367bb5de3103c87
punodsep <117001403+punodsep@users.noreply.github.com>
2025-10-30 18:16:45 +0700

    Home/Setting UI

--- Assets/Scripts/BinDestroy.cs
+++ Assets/Scripts/BinDestroy.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
 
-public class DestoryTemp : MonoBehaviour
+public class BinDestroy : MonoBehaviour
 {
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag != "Spoon" || collision.tag != "Pestle")
         { Destroy(collision.gameObject); }
+        SFXManager.Instance.PlaySFX("RubbishBin");
     }
 }
